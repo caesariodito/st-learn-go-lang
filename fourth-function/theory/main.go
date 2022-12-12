009422beaa8279090f6f17438d42e3f14435ee8a
Caesario Dito <caesariodito2014@gmail.com>
2022-12-12 17:48:48 +0700

    multiple returns on a function

--- fourth-function/theory/main.go
+++ fourth-function/theory/main.go
@@ -13,8 +13,13 @@ func main() {
 	helloWorld()
 	greet("Agus")
 	result := add(1,2)
-
 	fmt.Println(result)
+
+	// harus ditangkap semua, jika tidak butuh bisa gunakan syntax ini
+	// surface, _ := calculate(5,5)
+	surface, circumference := calculate(5,5)
+	fmt.Println(surface, circumference)
+
 }
 
 func helloWorld() {
@@ -29,4 +34,11 @@ func greet(name string) {
 // func add(a int, b int) int {
 func add(a, b int) int {
 	return a+b
+}
+
+// mulitple returns on a function
+func calculate(h, w int) (int, int) {
+	surface := h * w
+	circumference := 2 * (h + w)
+	return surface, circumference
 }
