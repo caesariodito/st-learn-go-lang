c62ae031e3296baef853b96f1007840c87b434bb
Caesario Dito <caesariodito2014@gmail.com>
2022-12-12 16:22:08 +0700

    finish slice of map

--- third-basic-golang/main.go
+++ third-basic-golang/main.go
@@ -1,13 +1,12 @@
 package main
 
-import "fmt"
-
 func main() {
 
 	// typedata.Array()
 	// typedata.Slice()
 	// typedata.MapPartOne()
 	// typedata.MapPartTwo()
+	// typedata.SliceOfMap()
 
 	
 }
--- third-basic-golang/typedata/slice.go
+++ third-basic-golang/typedata/slice.go
@@ -21,3 +21,35 @@ func Slice() {
 		fmt.Println(elem)
 	}
 }
+
+func SliceOfMap(){
+	// map dan slice bisa digabung
+	// di dalam slice terdapat 3 map
+	students := []map[string]string{
+		{"name": "Agung", "score": "A"},
+		{"name": "Link", "score": "B"},
+		{"name": "Mario", "score": "E"},
+	}
+
+	for _, student := range students {
+		// fmt.Println(student)
+		// map[name:Agung score:A]
+		// map[name:Link score:B]
+		// map[name:Mario score:E]
+
+		// fmt.Println(student["name"])
+		// Agung
+		// Link
+		// Mario
+
+		// fmt.Println(student["score"])
+		// A
+		// B
+		// E
+
+		fmt.Println(student["name"], "-", student["score"])
+		// Agung - A
+		// Link - B
+		// Mario - E
+	}
+}
