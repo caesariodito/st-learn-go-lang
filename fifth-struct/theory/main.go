570ce11e72e53f8a99a3336b42b4707db957e426
Caesario Dito <caesariodito2014@gmail.com>
2022-12-12 20:53:47 +0700

    embedded struct (struct didalam struct)

--- fifth-struct/theory/main.go
+++ fifth-struct/theory/main.go
@@ -10,15 +10,42 @@ type User struct {
 	IsActive  bool
 }
 
+type Group struct {
+	Name        string
+	Admin       User
+	Users       []User
+	IsAvailable bool
+}
+
 func main() {
 	// parts.CreateStructExample()
 
-	user := User{3, "Agus", "Susanto", "agus@gmail.com", true}
-	display := displayUser(user)
+	user2 := User{2, "Agus", "Susanto", "agus@gmail.com", true}
+	user3 := User{3, "Bambang", "Salim", "bambang@gmail.com", true}
+	display := displayUser(user2)
 	fmt.Println(display)
+
+	fmt.Println("===")
+
+	// users = []User{user2, user3}
+	// group := Group{"Gamer", user2, users, true}
+	group := Group{"Gamer", user2, []User{user2, user3}, true}
+	displayGroup(group)
 }
 
 func displayUser(user User) (result string) {
 	result = fmt.Sprintf("Name: %s %s, Email: %s", user.FirstName, user.LastName, user.Email)
 	return
+}
+
+func displayGroup(group Group) {
+	fmt.Printf("Name: %s", group.Name)
+	fmt.Println()
+	fmt.Printf("Member count: %d", len(group.Users))
+	fmt.Println("\nUser's name:")
+
+	for _, user := range group.Users {
+		fmt.Printf("%s %s", user.FirstName, user.LastName)
+		fmt.Println()
+	}
 }
