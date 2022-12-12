399f72f44f76ce54aba18ee453e240766cd03038
Caesario Dito <caesariodito2014@gmail.com>
2022-12-12 17:41:38 +0700

    basic function

--- fourth-function/theory/main.go
+++ fourth-function/theory/main.go
@@ -1,5 +1,30 @@
 package main
 
+import "fmt"
+
 func main() {
-	
+
+	/*
+		parameter/input
+		proses
+		output
+	*/
+
+	helloWorld()
+	greet("Agus")
+	result := add(1,2)
+
+	fmt.Println(result)
+}
+
+func helloWorld() {
+	fmt.Println("Hello, world!")
+}
+
+func greet(name string) {
+	fmt.Println("Hello,", name)
+}
+
+func add(a int, b int) int {
+	return a+b
 }
