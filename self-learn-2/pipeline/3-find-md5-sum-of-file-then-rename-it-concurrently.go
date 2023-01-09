dfce218861f579ffc23264df893562a2ba9115f3
synraax <caesariodito2014@gmail.com>
2023-01-09 18:32:58 +0700

    lil edit

--- self-learn-2/pipeline/3-find-md5-sum-of-file-then-rename-it-concurrently.go
+++ self-learn-2/pipeline/3-find-md5-sum-of-file-then-rename-it-concurrently.go
@@ -1,153 +1,169 @@
 package main
 
-import (
-	"crypto/md5"
-	"fmt"
-	"io/ioutil"
-	"log"
-	"os"
-	"path/filepath"
-	"sync"
-	"time"
-)
+// import (
+// 	"crypto/md5"
+// 	"fmt"
+// 	"io/ioutil"
+// 	"log"
+// 	"os"
+// 	"path/filepath"
+// 	"sync"
+// 	"time"
+// )
 
-var tempPath = filepath.Join(os.Getenv("TEMP"), "chapter-A.59-pipeline-temp")
+// var tempPath = filepath.Join(os.Getenv("TEMP"), "chapter-A.59-pipeline-temp")
 
-type FileInfo struct {
-	FilePath  string // file location
-	Content   []byte // file content
-	Sum       string // md5 sum of content
-	IsRenamed bool   // indicator whether the particular file is renamed already or not
-}
+// type FileInfo struct {
+// 	FilePath  string // file location
+// 	Content   []byte // file content
+// 	Sum       string // md5 sum of content
+// 	IsRenamed bool   // indicator whether the particular file is renamed already or not
+// }
 
-func main() {
+// func main() {
 
-	// runtime.GOMAXPROCS(8)
+// 	// runtime.GOMAXPROCS(8)
 
-	log.Println("start")
-	start := time.Now()
+// 	log.Println("start")
+// 	start := time.Now()
 
-	// pipeline 1: loop all files and read it
-	chanFileContent := readFiles()
+// 	// pipeline 1: loop all files and read it
+// 	chanFileContent := readFiles()
 
-	// pipeline 2: calculate md5sum
-	chanFileSum1 := getSum(chanFileContent)
-	chanFileSum2 := getSum(chanFileContent)
-	chanFileSum3 := getSum(chanFileContent)
-	chanFileSum4 := getSum(chanFileContent)
-	chanFileSum5 := getSum(chanFileContent)
-	chanFileSum := mergeChanFileInfo(chanFileSum1, chanFileSum2, chanFileSum3, chanFileSum4, chanFileSum5)
+// 	// pipeline 2: calculate md5sum
+// 	chanFileSum1 := getSum(chanFileContent)
+// 	chanFileSum2 := getSum(chanFileContent)
+// 	chanFileSum3 := getSum(chanFileContent)
+// 	chanFileSum4 := getSum(chanFileContent)
+// 	chanFileSum5 := getSum(chanFileContent)
+// 	chanFileSum := mergeChanFileInfo(chanFileSum1, chanFileSum2, chanFileSum3, chanFileSum4, chanFileSum5)
 
-	// pipeline 3: rename files
-	chanRename1 := rename(chanFileSum)
-	chanRename2 := rename(chanFileSum)
-	chanRename3 := rename(chanFileSum)
-	chanRename4 := rename(chanFileSum)
-	chanRename5 := rename(chanFileSum)
-	chanRename := mergeChanFileInfo(chanRename1, chanRename2, chanRename3, chanRename4, chanRename5)
+// 	// pipeline 3: rename files
+// 	chanRename1 := rename(chanFileSum)
+// 	chanRename2 := rename(chanFileSum)
+// 	chanRename3 := rename(chanFileSum)
+// 	chanRename4 := rename(chanFileSum)
+// 	chanRename5 := rename(chanFileSum)
+// 	chanRename := mergeChanFileInfo(chanRename1, chanRename2, chanRename3, chanRename4, chanRename5)
 
-	// print output
-	counterRenamed := 0
-	counterTotal := 0
-	for fileInfo := range chanRename {
-		if fileInfo.IsRenamed {
-			counterRenamed++
-		}
-		counterTotal++
-	}
+// 	// print output
+// 	counterRenamed := 0
+// 	counterTotal := 0
+// 	for fileInfo := range chanRename {
+// 		if fileInfo.IsRenamed {
+// 			counterRenamed++
+// 		}
+// 		counterTotal++
+// 	}
 
-	log.Printf("%d/%d files renamed", counterRenamed, counterTotal)
+// 	log.Printf("%d/%d files renamed", counterRenamed, counterTotal)
 
-	duration := time.Since(start)
-	log.Println("done in", duration.Seconds(), "seconds")
-}
-func readFiles() <-chan FileInfo {
-	chanOut := make(chan FileInfo)
+// 	duration := time.Since(start)
+// 	log.Println("done in", duration.Seconds(), "seconds")
+// }
+// func readFiles() <-chan FileInfo {
+// 	chanOut := make(chan FileInfo)
 
-	go func() {
-		err := filepath.Walk(tempPath, func(path string, info os.FileInfo, err error) error {
+// 	go func() {
+// 		err := filepath.Walk(tempPath, func(path string, info os.FileInfo, err error) error {
 
-			// if there is an error, return immediatelly
-			if err != nil {
-				return err
-			}
+// 			// if there is an error, return immediatelly
+// 			if err != nil {
+// 				return err
+// 			}
 
-			// if it is a sub directory, return immediatelly
-			if info.IsDir() {
-				return nil
-			}
+// 			// if it is a sub directory, return immediatelly
+// 			if info.IsDir() {
+// 				return nil
+// 			}
 
-			buf, err := ioutil.ReadFile(path)
-			if err != nil {
-				return err
-			}
+// 			buf, err := ioutil.ReadFile(path)
+// 			if err != nil {
+// 				return err
+// 			}
 
-			chanOut <- FileInfo{
-				FilePath: path,
-				Content:  buf,
-			}
+// 			chanOut <- FileInfo{
+// 				FilePath: path,
+// 				Content:  buf,
+// 			}
 
-			return nil
-		})
-		if err != nil {
-			log.Println("ERROR:", err.Error())
-		}
+// 			return nil
+// 		})
+// 		if err != nil {
+// 			log.Println("ERROR:", err.Error())
+// 		}
 
-		close(chanOut)
-	}()
+// 		close(chanOut)
+// 	}()
 
-	return chanOut
-}
+// 	fmt.Println("Reading Files...")
+// 	n := <-chanOut
+// 	fmt.Println(n.FilePath)
 
-func getSum(chanIn <-chan FileInfo) <-chan FileInfo {
-	chanOut := make(chan FileInfo)
+// 	return chanOut
+// }
 
-	go func() {
-		for fileInfo := range chanIn {
-			fileInfo.Sum = fmt.Sprintf("%x", md5.Sum(fileInfo.Content))
-			chanOut <- fileInfo
-		}
-		close(chanOut)
-	}()
+// func getSum(chanIn <-chan FileInfo) <-chan FileInfo {
+// 	chanOut := make(chan FileInfo)
 
-	return chanOut
-}
+// 	go func() {
+// 		for fileInfo := range chanIn {
+// 			fileInfo.Sum = fmt.Sprintf("%x", md5.Sum(fileInfo.Content))
+// 			chanOut <- fileInfo
+// 		}
+// 		close(chanOut)
+// 	}()
 
-func mergeChanFileInfo(chanInMany ...<-chan FileInfo) <-chan FileInfo {
-	wg := new(sync.WaitGroup)
-	chanOut := make(chan FileInfo)
+// 	fmt.Println("Calculating md5sum...")
+// 	n := <-chanOut
+// 	fmt.Println(n.FilePath)
 
-	wg.Add(len(chanInMany))
-	for _, eachChan := range chanInMany {
-		go func(eachChan <-chan FileInfo) {
-			for eachChanData := range eachChan {
-				chanOut <- eachChanData
-			}
-			wg.Done()
-		}(eachChan)
-	}
+// 	return chanOut
+// }
 
-	go func() {
-		wg.Wait()
-		close(chanOut)
-	}()
+// func mergeChanFileInfo(chanInMany ...<-chan FileInfo) <-chan FileInfo {
+// 	wg := new(sync.WaitGroup)
+// 	chanOut := make(chan FileInfo)
 
-	return chanOut
-}
+// 	wg.Add(len(chanInMany))
+// 	for _, eachChan := range chanInMany {
+// 		go func(eachChan <-chan FileInfo) {
+// 			for eachChanData := range eachChan {
+// 				chanOut <- eachChanData
+// 			}
+// 			wg.Done()
+// 		}(eachChan)
+// 	}
 
-func rename(chanIn <-chan FileInfo) <-chan FileInfo {
-	chanOut := make(chan FileInfo)
+// 	go func() {
+// 		wg.Wait()
+// 		close(chanOut)
+// 	}()
 
-	go func() {
-		for fileInfo := range chanIn {
-			newPath := filepath.Join(tempPath, fmt.Sprintf("file-%s.txt", fileInfo.Sum))
-			err := os.Rename(fileInfo.FilePath, newPath)
-			fileInfo.IsRenamed = err == nil
-			chanOut <- fileInfo
-		}
+// 	fmt.Println("Merging channels of calculation Md5sum ...")
+// 	n := <-chanOut
+// 	fmt.Println(n.FilePath)
 
-		close(chanOut)
-	}()
+// 	return chanOut
+// }
 
-	return chanOut
-}
+// func rename(chanIn <-chan FileInfo) <-chan FileInfo {
+// 	chanOut := make(chan FileInfo)
+
+// 	go func() {
+// 		for fileInfo := range chanIn {
+// 			newPath := filepath.Join(tempPath, fmt.Sprintf("file-%s.txt", fileInfo.Sum))
+// 			err := os.Rename(fileInfo.FilePath, newPath)
+// 			fileInfo.IsRenamed = err == nil
+// 			chanOut <- fileInfo
+// 		}
+
+// 		close(chanOut)
+// 	}()
+
+// 	fmt.Println("Renaming Files...")
+// 	n := <-chanOut
+// 	fmt.Println(n.FilePath)
+
+// 	return chanOut
+// }
